ff840713503e6df6f93c0f6dfd4da6d3606e4829
Rick <ricardo.henriques@accedo.tv>
2024-01-02 16:06:43 +0000

    some nice colors

---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const Colors = {
+  primary50: "#d9fdcf",
+  primary100: "#b1fba0",
+  primary200: "#8df877",
+  primary400: "#44f544",
+  primary500: "#53f01a",
+  primary700: "#22c905",
+  primary800: "#008810",
+  accent500: "#c90be6",
+  gray700: "#221c30",
+};
